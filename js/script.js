462d7d3e760bc41e0b6653a3f41e95f544735d4d
Max St <maxstouten@hotmail.com>
2019-06-16 19:01:03 +0200

    playing with fullpagejs

---
+++ js/script.js
@@ -0,0 +1,8 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    navigation: true,
+    anchors: ['section1', 'section2', 'section3'], //use this to navigate to the sections with an anchor tag in the html
+    navigationTooltips: ['Galaxy', 'Milky Way', 'Star Struck'],
+    showActiveTooltip: true,
+    scrollingSpeed: 700
+})
